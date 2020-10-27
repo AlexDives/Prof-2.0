60bb61f6cbfcbf042b9e4a5b64142e6925fc21a5
A-Div <small.ball.92@gmail.com>
2020-10-27 14:20:23 +0300

    update version 2.0.6.0

--- Prof/Properties/AssemblyInfo.cs
+++ Prof/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Организация: A-Div studio (alex.dives@yandex.ru)")]
 [assembly: AssemblyProduct("Продукт: Профсоюз")]
-[assembly: AssemblyCopyright("Авторские права: AlexDives ©  2019")]
+[assembly: AssemblyCopyright("Авторские права: AlexDives ©  2019-2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.5.0")]
-[assembly: AssemblyFileVersion("2.0.5.0")]
+[assembly: AssemblyVersion("2.0.6.0")]
+[assembly: AssemblyFileVersion("2.0.6.0")]
